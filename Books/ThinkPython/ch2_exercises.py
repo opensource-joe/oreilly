b17f74150a379a7c3783821261acc22980781119
opensource-joe <joe.castle@sas.com>
2022-08-27 10:37:14 -0400

    ch2 exercise content

---
+++ Books/ThinkPython/ch2_exercises.py
@@ -0,0 +1,23 @@
+# https://learning.oreilly.com/library/view/think-python-2nd/9781491939406/ch02.html#:-:text=Exercises
+
+# Exercise 2-1.
+# Repeating my advice from the previous chapter, whenever you learn a new feature, you should try it out in interactive mode and make errors on purpose to see what goes wrong.
+
+# We’ve seen that n = 42 is legal. What about 42 = n?
+
+# How about x = y = 1?
+
+# In some languages every statement ends with a semicolon, ;. What happens if you put a semicolon at the end of a Python statement?
+
+# What if you put a period at the end of a statement?
+
+# In math notation you can multiply x and y like this: . What happens if you try that in Python?
+
+# Exercise 2-2.
+# Practice using the Python interpreter as a calculator:
+
+# The volume of a sphere with radius r is . What is the volume of a sphere with radius 5?
+
+# Suppose the cover price of a book is $24.95, but bookstores get a 40% discount. Shipping costs $3 for the first copy and 75 cents for each additional copy. What is the total wholesale cost for 60 copies?
+
+# If I leave my house at 6:52 am and run 1 mile at an easy pace (8:15 per mile), then 3 miles at tempo (7:12 per mile) and 1 mile at an easy pace again, what time do I get home for breakfast?
