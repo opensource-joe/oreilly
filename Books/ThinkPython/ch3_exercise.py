d379977dce52affd58b4f1e249bcdd0f6f8e84f6
opensource-joe <joe.castle@sas.com>
2022-08-30 15:41:39 -0400

    update ch2, add ch3

---
+++ Books/ThinkPython/ch3_exercise.py
@@ -0,0 +1,12 @@
+# https://learning.oreilly.com/library/view/think-python-2nd/9781491939406/ch03.html#:-:text=Exercise%203-3.
+
+# Answers - https://github.com/AllenDowney/ThinkPython2/blob/master/code/grid.py
+
+# Exercise 3-3: Write a function that creates a 2 x 2 grid
+
+print('+', ('-' + ' ') * 4 + '+')
+print(('|' + '         ') * 4)
+
+
+
+# Exercise 3-4: Write a function that creates a 4 x 4 grid
